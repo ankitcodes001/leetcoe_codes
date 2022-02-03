d9a50b4d6cd10023a9ee5523ec487eb960861caf
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-03 16:54:29 +0530

    Added solution - LeetHub

--- Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
@@ -21,8 +21,8 @@ public:
 	  for(int i =2;i<=n;i++)
 	  {
 	      
-	    dp1[i] = ((dp1[i-1]%mod)+(dp0[i-1]%mod))%mod;
-	    dp0[i] = (dp1[i-1])%mod;
+	    dp0[i] = ((dp1[i-1]%mod)+(dp0[i-1]%mod))%mod;
+	    dp1[i] = (dp0[i-1])%mod;
 	  }
 	  
 	  return (dp1[n]+dp0[n])%mod;
