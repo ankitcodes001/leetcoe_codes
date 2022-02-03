4cd036ea01ddb5b4a7effa44bb7e0d12164296ca
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-03 16:55:44 +0530

    Added solution - LeetHub

--- Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
@@ -8,24 +8,23 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	 #define ll long long
+	// #define ll long long
 	ll countStrings(int n) {
 	  
 	  int mod = 1e9+7;
-	  int dp1[n+1];
-	  int dp0[n+1];
-	  dp1[1]=1;
-	  dp0[1]=1;
-	  
-	  
+	  int old_0=1;
+	  int old_1=1;
 	  for(int i =2;i<=n;i++)
 	  {
 	      
-	    dp0[i] = ((dp1[i-1]%mod)+(dp0[i-1]%mod))%mod;
-	    dp1[i] = (dp0[i-1])%mod;
+	      int new_1 = (old_0)%mod;
+	      int new_0 = ((old_1%mod)+(old_0%mod))%mod;
+	      
+	      old_1 = new_1;
+	      old_0 = new_0;
 	  }
 	  
-	  return (dp1[n]+dp0[n])%mod;
+	  return (old_1+old_0)%mod;
 	  
 	  
 	  
