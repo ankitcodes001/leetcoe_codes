cc9e7bbe9b91cac69fcf4383aab883e82c8684fd
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-07 08:34:34 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.11%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+    // memorization
+    // time complexity = n;
+    // space  = o(n)
+    
+   
+    int find(vector<int>&nums,int i,vector<int>&dp)
+    {
+        if(i>=nums.size())
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        
+      return  dp[i] = max(nums[i]+find(nums,i+2,dp),find(nums,i+1,dp));
+        
+    }
+    
     int rob(vector<int>& nums) {
+       
+       vector<int> dp(nums.size()+1,-1);
+    
+        int i  =0;
+        return find(nums,i,dp);
         
         
-        int inc = nums[0];
-        int exc = 0;
-        for(int i  = 1;i<nums.size();i++)
-        {
-         
-           int include = exc + nums[i];
-            int exclude = max(inc,exc);
-            inc = include;
-            exc = exclude;
-            
-            
-        }
+       
         
-        return max(exc,inc);
         
     }
 };
