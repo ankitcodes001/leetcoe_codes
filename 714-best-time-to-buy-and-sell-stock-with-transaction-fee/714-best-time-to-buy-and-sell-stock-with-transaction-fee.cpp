022b0541d739fb2996efeea67537606c7b5dd6ce
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-09 13:29:45 +0530

    Time: 149 ms (48.89%), Space: 59.3 MB (33.45%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
+    
+    int dp[50004][2];
+    
+    int find(vector<int>&a,int i,int bought,int fee)
+    {
+        if(i>=a.size())
+            return 0;
+        if(dp[i][bought]!= -1)
+            return dp[i][bought];
+        
+        
+        // ignoring or do nothing
+        
+        int ignore = find(a,i+1,bought,fee);
+        
+        // buy
+        
+        int val;
+        
+        if(bought)
+            val = (a[i]-fee)+find(a,i+1,false,fee);
+        else
+            val = -a[i]+find(a,i+1,true,fee);
+            
+            
+        
+        return dp[i][bought] = max(val,ignore);
+        
+        
+        
+        
+    }
+    
+    
     int maxProfit(vector<int>& a, int fee) {
+        // recursive with memo
         
-        int old_buy = -a[0];
-        int old_sell = 0;
-        for(int i  =1;i<a.size();i++)
-        {
-            int new_buy = max(old_sell-a[i],old_buy);
-            int new_sell= max((a[i]-fee)+old_buy, old_sell);
-            old_buy = new_buy;
-            old_sell= new_sell;
-        }
+        memset(dp,-1,sizeof(dp));
         
-        
-        return old_sell;
-        
+        return find(a,0,0,fee);
+      
         
     }
 };
