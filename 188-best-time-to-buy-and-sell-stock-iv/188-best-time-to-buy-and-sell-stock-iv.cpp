e29fad072ec9facff0f71919f141ad08078dc02f
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-09 12:48:36 +0530

    Time: 47 ms (26.70%), Space: 11.9 MB (34.21%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    
+    int dp[1000][101][2];
+    
+    int find(vector<int>&a,int i,int k,int check)
+    {
+        if(i>=a.size() || k<=0)
+            return 0;
+        
+        if(dp[i][k][check]!= -1)
+            return dp[i][k][check];
+       
+        int buy =0,not_buy =0,sell=0,not_sell=0;
+        if(check && k)
+         buy = find(a,i+1,k,!check)-a[i];
+         not_buy = find(a,i+1,k,check);
+        
+        if(!check && k)
+           sell = find(a,i+1,k-1,!check)+a[i];
+           not_sell = find(a,i+1,k,check);
+        
+        return dp[i][k][check] = max({sell,not_sell,buy,not_buy});
+      
+        
+    }
+  
+    
+    
+    
     int maxProfit(int k, vector<int>& a) {
         
         /* days
@@ -11,6 +39,40 @@ public:
         2   0   2    2
        
         */
+        
+        // recursive
+        
+        
+        
+        
+        int n = a.size();
+        
+        
+        if(n==0 || k ==0)
+            return 0;
+        memset(dp,-1,sizeof(dp));
+        
+        return find(a,0,k,1);
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        recursi
+        
         
         int n = a.size();
         if(n==0 || k==0)
@@ -29,24 +91,19 @@ public:
             }
         }
         
+        
+        
+        
+        // o(n2)
         for(int t =1;t<=k;t++)
         {
+            int maxi = INT_MIN;
+            
             for(int d = 1;d<n;d++)
             {
-                int maxi  = dp[t][d-1];
-                
-                for(int pd =0;pd<d;pd++)
-                {
-                   int previous_day_profit = dp[t-1][pd]; // k-1 transaction
-                    int transaction = a[d]-a[pd];
-                    
-                    maxi = max(previous_day_profit+transaction,maxi);
-                    
-                    
-                    
-                }
-                
-                dp[t][d] = maxi;
+                maxi = max(dp[t-1][d-1]- a[d-1],maxi);
+             
+                dp[t][d] = max(maxi+a[d],dp[t][d-1]);
                 
                 
             }
@@ -58,7 +115,7 @@ public:
         
         
         
-        
+        */
         
     }
 };
