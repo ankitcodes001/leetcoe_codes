75e15d1f800fbe37afaf75e3adf7fc52235db649
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-12 12:07:37 +0530

    Time: 215 ms (38.76%), Space: 13.2 MB (49.79%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -8,7 +8,7 @@ public:
        vector<int>dp(n,1),way(n,1);
         int ans= 1;
       
-        for(int i =1;i<n;i++)
+        for(int i =0;i<n;i++)
         {
             for(int j = 0;j<i;j++)
             { 
