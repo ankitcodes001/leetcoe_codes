969b0a946046d37f5a851cee9ecd6296e2336646
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-14 01:05:08 +0530

    Time: 10 ms (34.25%), Space: 8.4 MB (42.90%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         
-       
-       vector<int>dp(n+1);
-        dp[0]=0;
-        for(int i  =1;i<=n;i++)
-        {
-            
-            dp[i] = dp[i/2]+i%2;
-            
-        }
+      vector<int>dp;
+       for(int i =0;i<=n;i++)
+       {
+           
+           long long mask = 1L<<31;
+            int c= 0;
+           int num =i;
+           
+           while(mask)
+           {
+               if(mask & num)
+                   c++;
+               
+               mask = mask>>1;
+               
+           }
+           
+           dp.push_back(c);
+           
+       }
         
         return dp;
         
