fbcd6a0f13d82956abbbe3ae74e684f8b70aa6b4
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-02-15 15:09:49 +0530

    Time: 36 ms (35.61%), Space: 10.8 MB (60.74%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
+    
+    int dp[1002][1002];
+    
+    int find(string &s1, string &s2, int i ,int j)
+    {
+        if(i==0 || j==0)
+            return 0;
+        
+        if(dp[i][j]!= -1)
+            return dp[i][j];
+        
+        if(s1[i-1] == s2[j-1])
+            return dp[i][j] = 1+find(s1,s2,i-1,j-1);
+        else 
+            return dp[i][j] = max(find(s1,s2,i-1,j),find(s1,s2,i,j-1));
+      
+        
+        
+        
+        
+        
+    }
+    
     int longestCommonSubsequence(string s1, string s2) {
         
         // approach
         int n = s1.size();
         int m = s2.size();
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-       
         
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j =1;j<=m;j++)
-                
-            {
-                if(n == 0 || m==0)
-                    dp[i][j] =0;
-                
-                if(s1[i-1]==s2[j-1])
-                
-                    dp[i][j] = 1+dp[i-1][j-1];
-               
-                if(s1[i-1]!= s2[j-1])
-                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
-            }
-        }
+        memset(dp,-1,sizeof(dp));
         
-        return dp[n][m];
-        
-        
+        int z = find(s1,s2,n,m);
+        if(z== -1)
+            return 0;
+        return z;
         
         
     }
