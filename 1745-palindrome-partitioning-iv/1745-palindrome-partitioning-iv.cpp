50059ddf77962f5b7cb5bd47ba144248bd07e418
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-03-03 15:02:41 +0530

    Time: 487 ms (30.37%), Space: 17.3 MB (67.04%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool checkPartitioning(string s) {
+        int n = s.size();
+        
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        for(int g =0;g<n;g++)
+        {
+            for(int i =0,j=g;j<n;i++,j++)
+            {
+                if(g==0)
+                    dp[i][j] = true;
+                else if(g==1)
+                {
+                    if(s[i]== s[j])
+                        dp[i][j]=true;
+                }
+                else
+                {
+                    if(s[i]==s[j] && dp[i+1][j-1])
+                    {
+                        dp[i][j]=true;
+                    }
+                    else
+                    {
+                        dp[i][j]=false;
+                    }
+                }
+                
+            }
+
+        }
+         
+        // check palindromes for i to j where i =0 and j = n-1
+        // and to check existence of three non empty subtrings we need k variable
+        // (i to k) and (k+1 to j)
+        
+      for(int i = 0; i < n - 2; ++i) {
+            for(int j = i + 1; j < n - 1; ++j) {
+                if(dp[0][i] && dp[i + 1][j] && dp[j + 1][n - 1]) return true;
+            }
+        }
+        
+        return false;
+        
+        
+        
+        
+        
+        
+    }
+};
