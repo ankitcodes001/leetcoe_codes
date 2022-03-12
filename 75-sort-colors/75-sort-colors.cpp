0a64cb2fce7958a68ecaff9a0dfc042abfc502b2
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-03-12 16:19:06 +0530

    Time: 4 ms (46.52%), Space: 8.3 MB (24.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
+    void sortColors(vector<int>& nums) {
         
-        /*
+        
         int zero =0;
         int one =0;
         int two =0;
@@ -22,52 +22,25 @@ public:
                 two++;
             }
         }
-        
-        int j =0;
-    for(int i =0;i<n;i++)
-    {
-        if(zero >0)
-        {
-        while(zero>0)
-        {
-            nums[j] = 0;
-            zero--;
-            j++;
-            
-        }
-        }
-        else if(one >0)
-        {
-        while(one>0)
-        {
-            nums[] =
-        }
-        
-            
-            
-        }
-    }
-     
-     */
-        int n = a.size();
-        
-        for(int i =0;i<n-1;i++)
-        {
-            for(int j = 0;j<n-i-1;j++)
-            {
-                
-                if(a[j] > a[j+1])
-                {
-                    swap(a[j],a[j+1]);
-                }
-                
-                
-            }
-            
-        }
-        
+
+        int i =0;
+        int j =zero;
+        int k =two;
     
-        
-        
+       
+        for(int i =0;i<zero;i++)
+        {
+            nums[i] =0;
+        }
+        for(int i =zero;i<zero+one;i++)
+        {
+            nums[i] =1;
+        }
+        for(int i =one+zero;i<nums.size();i++)
+        {
+            nums[i]=2;
+        }
+       
     }
+    
 };
