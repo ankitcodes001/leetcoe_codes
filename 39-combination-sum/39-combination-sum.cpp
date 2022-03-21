73a0f127a9c5e2172095beee0c4f076169548602
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-03-21 16:02:19 +0530

    Time: 8 ms (73.46%), Space: 11.1 MB (52.00%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>>v;
+    vector<int>temp;
+    void fun(vector<int>&a,int t,int i)
+    {
+        if(t <0 || i == a.size())
+        {
+            
+            return ;
+        }
+        
+        if(t == 0)
+        {v.push_back(temp);
+        return;
+         }
+        
+        
+        
+        temp.push_back(a[i]);
+        fun(a,t-a[i],i);
+        
+        temp.pop_back();
+        fun(a,t,i+1);
+        
+        
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& a, int t) {
+        //sort(a.begin(),a.end());
+        fun(a,t,0);
+        return v;
+    }
+};
