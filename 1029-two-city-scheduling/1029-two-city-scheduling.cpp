23bf6c6f08d7fcc46a150e1c05c3ac965757aa3b
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-03-25 10:58:51 +0530

    Time: 26 ms (5.79%), Space: 12.3 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-   
-    int twoCitySchedCost(vector<vector<int>>& nums) {
-    sort(nums.begin(),nums.end(),[](vector<int>&a,vector<int>&b){
+    int dp[101][101][101];
+    
+    
+    
+    int fun(int i ,int a,int b ,vector<vector<int>>&nums)
+    {
+        if(i>=nums.size())return 0;
+         int op1 = 1e9,op2 = 1e9;
         
-        return a[0]-b[0]<a[1]-b[1];
-    });
+        if(dp[i][a][b]!=-1)
+        return dp[i][a][b];
         
-        
-        //sort diff of cost of travelling from city a to city b
-        
-        
-        int ans  = 0;
-        int n = nums.size();
-        for(int i =0;i<n/2;i++)
+       
+        if(a)
         {
-            ans+= nums[i][0]+nums[n-i-1][1];
+            op1 = nums[i][0]+fun(i+1,a-1,b,nums);
             
             
         }
-    return ans;
+        if(b)
+        {
+            
+            op2 = nums[i][1]+fun(i+1,a,b-1,nums);
+        }
+            
+       return dp[i][a][b] = min(op1,op2);
+        
+       
+        
+        
+        
+    }
+    
+    int twoCitySchedCost(vector<vector<int>>& nums) {
+    memset(dp,-1,sizeof(dp));
+        
+    int a = nums.size()/2;
+    int b = nums.size()/2;
+    
+        return fun(0,a,b,nums);
+    
         
         
         
