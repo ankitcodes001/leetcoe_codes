a6394250edb9de29b52a50de09a3243a492f9c0b
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-03-28 10:22:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.54%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
+    
+    int fun(int n ,int k)
+    {
+        if(n==0)
+            return 1;
+        
+        return (fun(n-1,k)+k)%n;
+        
+        
+        
+        
+    }
+    
     int findTheWinner(int n, int k) {
-    queue<int>q;
-        for(int i = 1;i<=n;i++)
-            q.push(i);
+        /*
         
-        while(q.size()!=1)
-        {
-            int x= k;
-            while(x>1)
-            {
-                
-                int a = q.front();
-                q.push(a);
-                q.pop();
-                x--;
-            }
-            
-            q.pop();
-            
-        }
+         If you observe, you can see that we are doing the same activity again and again i.e. deleting every kth element we encounter. Therefore, we can think towards the recursive approach also.
+Intution : If I have the winner for 'n-1' and 'k'. I can find the winner for 'n' and 'k' by moving on to the next kth person (i.e. f(n) = f(n-1)+k). Keeping in mind that they are sitting on a round table we have to rotate also (i.e. f(n) = (f(n-1)+k)%n).
         
+        */
         
-        return q.front();
-        
-        
+        int ans = fun(n,k)+1;
+        return ans;
+    
     }
 };
