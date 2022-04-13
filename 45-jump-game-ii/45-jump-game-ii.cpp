884a617276b40a62b0e8412b81239c1d6d254039
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-04-13 11:04:14 +0530

    Time: 19 ms (76.24%), Space: 16.6 MB (52.10%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,59 +1,26 @@
 class Solution {
 public:
-    /*
-    long long fun(vector<int>&nums,int curr,vector<long long>&dp,int des)
-    {
-        if(curr == des)
-            return 0;
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int curr_reach =0;
+        int max_reach =0;
         
-        if(dp[curr]!=-1)
-            return dp[curr];
+        int step = 0;
         
-       long long  temp  = INT_MAX;
-        for(int i = 1;i<=nums[curr];i++)
+        for(int i =0;i<n-1;i++)
         {
-            if(curr+i>des)
-                break;
+            max_reach = max(nums[i]+i,max_reach);
             
-            temp = min(temp,1+fun(nums,curr+i,dp,des));
-             
+            if(i == curr_reach)
+            {
+                step++;
+                
+                curr_reach = max_reach;
+            }
+            
+            
         }
         
-        dp[curr] = temp;
-        
-        return temp;
-        
-        
-        
-        
-        
-    }
-    
-    */
-    
-    int jump(vector<int>& nums) {
-        //greedy ---> o(n)
-        // space  =  o(1)
-        
-        int  n = nums.size();
-        int des = n-1;
-        
-     int max_reach  =0 ;
-     int curr = 0;
-     int step =0 ;
-     for(int i = 0;i<n-1;i++)
-     {
-         max_reach = max(nums[i]+i,max_reach);
-         
-         if(i == curr)
-         {
-             step++;
-             curr = max_reach;
-         }
-         
-         
-         
-     }
         return step;
     }
 };
