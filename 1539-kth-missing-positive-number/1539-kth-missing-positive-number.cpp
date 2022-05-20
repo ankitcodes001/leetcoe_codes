d8e0c073a79e993e341947b13e33baacf0a6a5c8
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-05-20 06:04:51 +0530

    Time: 12 ms (19.50%), Space: 9.6 MB (28.67%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -8,10 +8,17 @@ public:
         
         int low =  0;
         int high = arr.size()-1;
+        
+        // a[i]-i-1 total missed elements till ith index
+        // we can a decision on the basis of this
+        // if  k > missed  move right
+        //  else move left
+        // return  low+k      
+        
         while(low <=  high)
         {
             int mid = low+(high -low)/2;
-            if(arr[mid]-mid-1 < k)
+            if(arr[mid]-mid-1 < k) // if  missed element is greater than k
             {
                 low = mid+1;
                 
