be6d04cdb35c81053fd06620c351a84369b591f2
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-06-13 13:53:13 +0530

    Time: 114 ms (70.96%), Space: 14.2 MB (69.57%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,38 +1,19 @@
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& mat) {
-        set<pair<int,int>>s;
-        int n = mat.size();
-        int m = mat[0].size();
-        for(int i =0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(mat[i][j] == 0)
-                {
-                    s.insert({i,j});
-                }
-                
-            }
-                
-        }
+class Solution(object):
+    def setZeroes(self, mat):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(mat)
+        m = len(mat[0])
+        r ,c = set(),set()
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    c.add(j)
+                    r.add(i)
         
-      for(auto  it : s)
-      {
-          int r = it.first;
-          int c = it.second;
-          
-          for(int i = 0;i<n;i++)
-          {
-              mat[i][c] = 0;
-          }
-          for(int i = 0;i<m;i++)
-          {
-              mat[r][i] = 0;
-          }
-          
-      }
-        
-        
-    }
-};
+        for i in range(n):
+            for j in range(m):
+                if i in r or j in c:
+                    mat[i][j] = 0
