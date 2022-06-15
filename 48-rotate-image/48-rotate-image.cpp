0c05e809b5a24398b9776110a901a732f2e2d9a3
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-06-15 18:54:36 +0530

    Time: 0 ms (100.00%), Space: 7 MB (92.48%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
         
+        // transponse swap(a[i][j],a[j][i]) for j<i  (col<row)
         int n = mat.size();
         
-        vector<vector<int>>v(n,vector<int>(n,0));
-        for(int i =0;i<n;i++)
+        for(int i = 0;i<n;i++)
         {
-            for(int j = 0;j<n;j++)
+            for(int j  = 0;j<i;j++)
             {
-                
-                v[j][n-i-1] = mat[i][j];
-                
-                
+                swap(mat[i][j],mat[j][i]);
             }
         }
         
-        mat = v;
-        
+        for(int i = 0;i<n;i++)
+        {
+            reverse(mat[i].begin(),mat[i].end());
+        }
         
         
         
