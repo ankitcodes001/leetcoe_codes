5a71c931d455f37ad036eb9d941c55413ef1d0e8
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-06-15 11:28:10 +0530

    Time: 4 ms (42.36%), Space: 8.3 MB (24.00%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,46 +1,83 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& a) {
         
         
-        int zero =0;
-        int one =0;
-        int two =0;
+//         int zero =0;
+//         int one =0;
+//         int two =0;
         
-        for(int i =0;i<nums.size();i++)
+//         for(int i =0;i<nums.size();i++)
+//         {
+//             if(nums[i] == 0)
+//             {
+//                 zero++;
+//             }
+//             else if(nums[i] == 1)
+//             {
+//                 one++;
+//             }
+//             else
+//             {
+//                 two++;
+//             }
+//         }
+
+//         int i =0;
+//         int j =zero;
+//         int k =two;
+    
+       
+//         for(int i =0;i<zero;i++)
+//         {
+//             nums[i] =0;
+//         }
+//         for(int i =zero;i<zero+one;i++)
+//         {
+//             nums[i] =1;
+//         }
+//         for(int i =one+zero;i<nums.size();i++)
+//         {
+//             nums[i]=2;
+//         }
+   
+     
+        int n = a.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        while(mid <= high)
         {
-            if(nums[i] == 0)
+            if(a[mid] == 0)
             {
-                zero++;
+                swap(a[mid],a[low]);
+                mid++;
+                low++;
             }
-            else if(nums[i] == 1)
+            else if(a[mid] == 1)
             {
-                one++;
+                mid++;
             }
             else
             {
-                two++;
+                swap(a[mid],a[high]);
+                high--;
             }
+            
+            
         }
-
-        int i =0;
-        int j =zero;
-        int k =two;
-    
-       
-        for(int i =0;i<zero;i++)
-        {
-            nums[i] =0;
-        }
-        for(int i =zero;i<zero+one;i++)
-        {
-            nums[i] =1;
-        }
-        for(int i =one+zero;i<nums.size();i++)
-        {
-            nums[i]=2;
-        }
-       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 };
