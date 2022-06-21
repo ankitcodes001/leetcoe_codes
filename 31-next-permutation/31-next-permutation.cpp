aee10667a0d2e7ba6e002f3afe78b4532c3f80de
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-06-21 07:39:25 +0530

    Time: 11 ms (23.03%), Space: 12.1 MB (74.68%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     void nextPermutation(vector<int>& a) {
     // traverse from end to start
-    // find the num[i] > num[i-1] first greater element from end
-    // index1 < 0 reverse    
-    // find the index of element which is greater than this element
-        // a[i] > a[index]
+    // find first decreasing order element from right
+    // find element first greater elemet than breakpoint
     //->swap both element and reverse(index+1, last)
-    
+    // and since we know that elemet after breakpoint so simply revese;    
+    // if(a.size() == 1 || a.size() == 0)
+    // {
+    //     return;
+    // }
+        
         
     int index1 = -1;
     for(int i  = a.size()-1;i>0;i--)
@@ -35,6 +38,7 @@ public:
         }
        }
         
-        
+    
+    
     }
 };
