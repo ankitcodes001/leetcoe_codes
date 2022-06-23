e0413126878edc8ce6baa818ee4b79a63e939553
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-06-23 21:30:01 +0530

    Time: 61 ms (30.51%), Space: 9.9 MB (62.17%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    
-    vector<vector<int>>v;
-    vector<int>temp;
-    
-    void fun(int n,int k, int i)
+    vector<vector<int>>ans;
+    vector<int>curr;
+    void fun(int n,int start,int k)
+    { if(curr.size() == k)
     {
-        
-        if(k == 0)
-        {
-            v.push_back(temp);
-            return;
-            
-        }
-        
-        if(i>n)return;
-
-        temp.push_back(i);
-        fun(n,k-1,i+1);
-        
-     
-        temp.pop_back();
-        fun(n,k,i+1);
-        
-        
+       ans.push_back(curr);
+      return ;  
     }
+        for(int i = start;i<=n;i++)
+        {
+            curr.push_back(i);
+            fun(n,i+1,k);
+            curr.pop_back();
+        }
+            
+        
     
+    }
     vector<vector<int>> combine(int n, int k) {
+    
+     
+     fun(n,1,k);
+     return ans;
         
-        fun(n,k,1);
-        
-        return v;
         
     }
 };
