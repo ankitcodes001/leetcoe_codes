9e80c1755e193dbd5e25207e05ed584996d38526
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-03 14:42:19 +0530

    Time: 4 ms (56.21%), Space: 7.2 MB (43.94%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+    // check the maximum number of valleys and peaks in array
+        
+     int pos = 1,neg =1;
+     for(int i = 1;i<nums.size();i++)
+     {
+         if(nums[i] > nums[i-1])
+             pos = neg+1;
+         else if (nums[i] < nums[i-1])
+             neg = pos+1;
+       
+     }
+   
+ return max(pos,neg);
+   
+    
+        
+        
+        
+        
+        
+    }
+};
