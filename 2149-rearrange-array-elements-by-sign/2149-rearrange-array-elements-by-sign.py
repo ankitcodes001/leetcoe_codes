3083b60b31710e721eb7c3be8e0cfc859447da08
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-03 13:59:51 +0530

    Time: 2700 ms (25.33%), Space: 45.8 MB (10.39%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,33 +1,14 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        
-        
-      vector<int>a,b;
-      for(auto x : nums)
-      {
-          if(x>0)
-          {
-              a.push_back(x);
-          }
-          else
-          {
-              b.push_back(x);
-          }
-      }
-        
-       vector<int>ans;
-        int i = 0;
-        int j = 0;
-        int n = a.size();
-        int m = b.size();
-        while(i<n && j<m)
-        {
-            ans.push_back(a[i++]);
-            ans.push_back(b[j++]);
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        post,neg,ans = [],[],[]
+        for ele in nums:
+            if ele < 0:
+                neg.append(ele)
+            else :
+                post.append(ele)
             
-        }
-        return ans;
+        for a in range(len(post)):
+            ans.append(post[a])
+            ans.append(neg[a])
             
-    }
-};
+        return ans    
