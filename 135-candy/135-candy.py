43da101ddaacc7348b77b30dfbf07addeaa8781c
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-04 08:43:00 +0530

    Time: 220 ms (62.95%), Space: 16.7 MB (74.92%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -1,34 +1,20 @@
-class Solution {
-public:
-    int candy(vector<int>& a) {
+class Solution:
+    def candy(self, a: List[int]) -> int:
+           
+     # distribute 1 candies to all 
+     # add +1 candy to each child having greater rating than from next elemt
+     # do simmilar from right side add max(candi[i],cand[i+1]+1);
+     # sum of all candies
+        n = len(a)
+        ans = [1]*n
         
-    // distribute 1 candies to all 
-    // add +1 candy to each child having greater rating than from next elemt
-   //  do simmilar from right side add max(candi[i],cand[i+1]+1);
-   // sum of all candies
-        
-   vector<int>ans(a.size(), 1);
-        
-   for(int i = 1;i<a.size();i++)
-   {
-       if(a[i] > a[i-1])
-       {
-           ans[i] = ans[i-1]+1; // right side
-       }
-   }
-   for(int i = a.size()-2;i>=0;i--)
-   {
-       if(a[i] > a[i+1]) // leftside
-       {
-           ans[i] = max(ans[i],ans[i+1]+1);
-       }
-   }
-   int sum =0;     
-   for(auto x : ans)
-       sum+=x;
-        
-   return sum;     
-    }
+        for i in range(1,len(a)):
+            if a[i] > a[i-1] : #right side
+                ans[i] = ans[i-1]+1
+        for i in range(n-2,-1,-1):
+            if a[i] > a[i+1]:
+                ans[i] = max(ans[i],ans[i+1]+1)
+            
         
         
-};
+        return sum(ans)
