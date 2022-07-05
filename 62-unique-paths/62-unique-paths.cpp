8b57cd7830cebc6579d7d16173ef24fb388282cd
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-05 15:22:51 +0530

    Time: 3 ms (43.80%), Space: 6 MB (73.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
+    int dp[101][101];
+    int countPath(int x,int y,int n, int m)
+    {
+        if(x>=n || y >= m )
+            return 0;
+        if(x == n-1 && y == m-1)
+            return 1;
+        if(dp[x][y]!= -1)
+            return dp[x][y];
+        else
+            dp[x][y] = countPath(x+1,y,n,m)+countPath(x,y+1,n,m);
+        return dp[x][y];
+    }
+    
+    
+      
+    
+    
     int uniquePaths(int m, int n) {
         
         // Since the robot can only move right and down, when it arrives at a point, it either arrives from left or above. If we use dp[i][j] for the number of unique paths to arrive at the point (i, j), then the state equation is dp[i][j] = dp[i][j - 1] + dp[i - 1][j]. Moreover, we have the base cases dp[0][j] = dp[i][0] = 1 for all valid i and j respectively.
         
-        int path[m][n];
-        for(int i =0;i<m;i++)
-        {
-            path[i][0] = 1;
-        }
-        for(int i =0;i<n;i++)
-        {
-            path[0][i] = 1;
-        }
-        
-        for(int i =1;i<m;i++)
-        {
-            for(int j = 1;j<n;j++)
-            {
-                
-                path[i][j] = path[i-1][j]+path[i][j-1];
-                
-            }
-        }
-        
-        return path[m-1][n-1];
+        memset(dp,-1,sizeof(dp));
+        return countPath(0,0,m,n);
         
         
         
