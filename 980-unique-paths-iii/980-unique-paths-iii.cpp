f65950545d506bd5352457770a449c976d4cd38a
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-06 14:42:49 +0530

    Time: 6 ms (48.14%), Space: 7.1 MB (65.08%) - LeetHub

---
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+ 
+        
+    int count_path(vector<vector<int>>&a,int x,int y,int count)
+    {  
+        if(x<0 || y<0 || x >= a.size() || y>= a[0].size() || a[x][y] == -1 )
+            return 0;
+        
+        if(a[x][y] == 2)
+        {
+            if(count == -1)
+                return 1;
+            return 0;
+        }
+        
+        a[x][y] = -1;
+        
+        int sum = 0;
+        
+        
+        sum+=count_path(a,x,y+1,count-1);
+        sum+=count_path(a,x,y-1,count-1);
+        sum+=count_path(a,x+1,y,count-1);
+        sum+=count_path(a,x-1,y,count-1);
+        
+        a[x][y] = 0;
+        return sum;
+        
+    
+        
+    
+    }
+    
+    int uniquePathsIII(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        int sx = -1,sy = -1;
+        int count = 0;
+      
+         for(int i = 0;i<n;i++)
+         {
+            for(int j = 0;j<m;j++)
+            {
+                if(a[i][j] == 1)
+                { 
+                    sx = i;
+                    sy = j;
+                }
+                else if(a[i][j] == 0)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        
+               
+        return count_path(a,sx,sy,count);
+        
+        
+        
+        
+    }
+};
