2732263c1ac05b09338b743106a89b7e4f98556a
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-08 07:08:30 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,50 +9,49 @@ using namespace std;
 
 class Solution{   
 public:
-   
-   bool check(vector<int>&a,int s)
-   {
-      int n = a.size();
-      
-      vector<vector<int>>dp(n+1,vector<int>(s+1));
-      for(int i =0;i<=n;i++)
-      {
-          for(int j =0;j<=s;j++)
-          {
-              if(i == 0)
-              dp[i][j] = 0;
-              if(j ==0)
-              dp[i][j] =1;
-              
-              
-          }
-      }
-      
-      for(int i= 1;i<=n;i++)
-      {
-          for(int j = 1;j<=s;j++)
-          {
-              if(a[i-1]<=s)
-              {
-                  dp[i][j] = dp[i-1][j-a[i-1]] || dp[i-1][j];
-              }
-              else
-              {
-                  dp[i][j] = dp[i-1][j];
-              }
-          }
-      }
-       
-       
-       return dp[n][s];
-       
-   }
-
-
-    bool isSubsetSum(vector<int>arr, int sum){
+    bool isSubsetSum(vector<int>a, int s){
+        // code here 
+        int n = a.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(s+1));
+        for(int i =0;i<=n;i++)
+        {
+            for(int j =0; j<=s;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j] = false;
+                }
+                if(j==0)
+                {
+                    dp[i][j] = true;
+                }
+                
+            }
+        }
+        
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=s;j++)
+            {
+                
+                if(a[i-1]<=j)
+                {
+                    
+                    dp[i][j] = dp[i-1][j]||dp[i-1][j-a[i-1]];
+                
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+        }
         
         
-        return check(arr,sum);
+        
+        
+        return dp[n][s];
         
         
     }
