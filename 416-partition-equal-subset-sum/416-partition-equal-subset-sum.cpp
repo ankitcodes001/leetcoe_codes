b563ef6107be36f808e1f78837f9eabccbfffb22
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-08 11:02:36 +0530

    Time: 561 ms (16.67%), Space: 11.9 MB (64.21%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
     
-    
-    bool checkSum(int s,vector<int>&nums)
-    {
-        int n = nums.size();
-        bool dp[n+1][s+1];
-        for(int i =0;i<=n;i++)
-        {  for(int j =0;j<=s;j++)
+    bool subset(vector<int>&a,int sum)
+    {   int n = a.size();
+        int s = sum;
+        vector<vector<bool>>dp(n+1,vector<bool>(s+1));
+        for(int i = 0;i<=n;i++)
+        {
+            for(int j = 0;j<=s;j++)
             {
                 if(i == 0)
-                    dp[i][j] = false;
-                if(j == 0)
-                   dp[i][j] = true; 
-            }
-        }
-        
-        
-        for(int i =1;i<=n;i++)
-        {
-            for(int j = 1;j<=s;j++)
-            {
-                
-                if(nums[i-1]<=j)
                 {
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    dp[i][j] = false;
                 }
-                else
-                    dp[i][j] = dp[i-1][j];
-                
-                
+                if(j == 0)
+                {
+                dp[i][j] = true;
+                }
             }
         }
+        
+     for(int i = 1;i<=n;i++)
+     {
+         for(int j = 1;j<=s;j++)
+         {
+             if(a[i-1]<=j)
+             {
+                 dp[i][j] = dp[i-1][j]||dp[i-1][j-a[i-1]];
+             }
+             else
+                 dp[i][j] = dp[i-1][j];
+         }
+     }
         
         return dp[n][s];
-        
         
     }
     
     bool canPartition(vector<int>& nums) {
         
-        int sum = 0;
-        for(auto x : nums) 
+        int sum  = 0;
+        for(int i = 0;i<nums.size();i++)
         {
-            sum+=x;
-        
+            sum+=nums[i];
         }
-        if(sum%2 == 1)
+        
+        if(sum%2!=0)
             return false;
-        else
-        {
-            return (checkSum(sum/2,nums));
-          
-        }
-        
+        return subset(nums,sum/2);
         
     }
 };
