7bb2d7449167c390e30849355e4722a1f24887af
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-08 06:54:56 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,47 +8,41 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
- 
-    
-    int fun(int w ,int wt[],int val[],int n)
-    {
-        vector<vector<int>>dp(n+1,vector<int>(w+1));
-        if(n==0 || w== 0)
-        return 0;
-        
-        for(int i = 0;i<=n;i++)
-        {
-            for(int j  = 0;j<=w;j++)
-            {
-                if(i==0 || j==0)
-                {
-                    dp[i][j] = 0;
-                }
-                else if(wt[i-1] <= j)
-                {
-                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
-                    
-                    
-                }
-                else
-                dp[i][j] = dp[i-1][j];
-                
-                
-                
-            }
-        }
-        
-        
-        return dp[n][w];
-        
-    }
     int knapSack(int w, int wt[], int val[], int n) 
     { 
-        
-        return fun(w,wt,val,n);
        
-     
+       vector<vector<int>>dp(n+1,vector<int>(w+1));
+       if(n==0 || w == 0)
+       return 0;
        
+       for(int i = 0;i<=n;i++)
+       {
+           for(int j = 0;j<=w;j++)
+           {
+               if(i==0 || j==0)
+               {
+                   dp[i][j] = 0;
+               }
+               else
+               {
+                   if(wt[i-1]<=j)
+                   {
+                       
+                       dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                       
+                       
+                   }
+                   else
+                   {
+                       dp[i][j] = dp[i-1][j];
+                   }
+               }
+               
+               
+           }
+       }
+       
+       return dp[n][w];
        
        
     }
