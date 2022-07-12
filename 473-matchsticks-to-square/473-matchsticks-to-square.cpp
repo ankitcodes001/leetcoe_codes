08c4d5618cd086b333cef1814d478deaf8a02174
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-12 15:09:28 +0530

    Time: 626 ms (35.63%), Space: 9.9 MB (84.07%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public: bool flag;
+    unordered_map<int,bool>mp;
+    bool solve(vector<int>&m,vector<int>&side,int i)
+    {   
+//         if(mp.find(i)!= mp.end())
+//             return mp[i];
+        
+        if(i==m.size())
+        {
+         return side[0] == 0 && side[1] == 0 && side[2] == 0 && side[3] == 0;
+        }
+        
+        for(int j =0;j<side.size();j++)
+        {  if(m[i]>side[j]) continue;
+            side[j]-=m[i];
+            if(solve(m,side,i+1))
+            {
+                return true;
+            }
+             side[j]+=m[i];
+        }
+        
+        return false;
+        
+    }
+    bool makesquare(vector<int>& m) {
+        if(m.size()<4)
+            return false;
+        int sum = 0;
+        for(auto x : m)
+            sum+=x;
+        
+     if(sum%4!=0)
+         return false;
+     int s = sum/4;
+     vector<int>side(4,s);
+     sort(m.begin(),m.end(),greater<int>());   
+     return solve(m,side,0);   
+        
+  
+        
+        
+    }
+};
