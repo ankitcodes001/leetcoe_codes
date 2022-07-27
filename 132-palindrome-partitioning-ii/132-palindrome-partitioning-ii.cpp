a64d6d6b80a85c321975d68f9358d4d217769b13
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-27 15:04:24 +0530

    Time: 607 ms (30.29%), Space: 24.1 MB (37.79%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,58 +1,59 @@
 class Solution {
 public:
-    
-    bool isPal(string &s,int i ,int j)
+     int dp[2001][2001];
+    bool is_pal(string &s,int i, int j)
     {
-        
         if(i==j)
             return true;
         while(i<=j)
         {
             if(s[i]!=s[j])
                 return false;
-                i++;
+            i++;
             j--;
         }
+        
         return true;
         
+        
     }
-    
-    int dp[2001][2001];
-    int fun(string &s,int i ,int j)
+    int solve(string &s,int i,int j)
     {
-        if(i>=j)
+        if(i >=j)
             return 0;
-        if(isPal(s,i,j))
+        if(is_pal(s,i,j))
         {
-            dp[i][j] =0;
-            return 0;
+            return dp[i][j] = 0;
         }
         if(dp[i][j]!= -1)
             return dp[i][j];
         
-        int ans = INT_MAX;
-        for(int k =i;k<j;k++)
+        int ans  = INT_MAX;
+        for(int k = i;k<j;k++)
         { int temp;
-             if(isPal(s,i,k))
-             {
-                 temp=1+fun(s,k+1,j);
-             }
-            ans= min(temp,ans);
+            if(is_pal(s,i,k))
+            {
+                temp =  1+solve(s,k+1,j);
+            }
+          ans  = min(temp,ans);
+            
+             
         }
+         return dp[i][j] = ans;
         
-        return dp[i][j]=ans;
+        
         
     }
-    
-    
     int minCut(string s) {
-        int n = s.size();
-        memset(dp,-1,sizeof(dp));
-        int i =0;
+    int n  = s.size();
         
-        int j =n-1;
+    memset(dp, -1,sizeof(dp));
+    return solve(s,0,s.size()-1);
+         
+         
+         
         
-        return fun(s,i,j);
+        
         
         
         
