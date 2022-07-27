d399aafc5be8e44cda9de88c03d983f8cbbfa106
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-27 23:18:35 +0530

    Time: 5 ms (65.43%), Space: 7.6 MB (70.68%) - LeetHub

---
+++ 877-stone-game/877-stone-game.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+//     int dp[501];
+//     bool solve(vector<int>&nums,int i,int j)
+//     {
+       
+        
+           
+            
+        
+        
+        
+        
+        
+//     }
+    bool stoneGame(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         if(n==0)
+//             return false;
+//         if(n == 1)
+//             return true;
+//         if(n == 2)
+//             return nums[0]>nums[1];
+        
+//        int a  = 0,b=0;
+            
+//         while(nums.size() > 0 )
+//         {
+//             a+= max(*nums.begin(),*nums.end());
+//             if(*nums.begin()>*nums.end())
+//              nums.erase(nums.begin());
+//             else
+//              nums.pop_back();
+           
+//            b+=max(*nums.begin(),*nums.end());
+//            if(*nums.begin()>*nums.end())
+//               nums.erase(nums.begin());
+//             else
+//              nums.pop_back();
+//         }
+//         memset(dp, -1,sizeof(dp));
+//         return solve(nums,0,n-1);
+        
+//         return a>b;
+return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
