d9bf00c2b6aa4fa8bed677f135b13c5ffbf4fc41
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-07-29 15:19:08 +0530

    Time: 20 ms (65.48%), Space: 10.6 MB (82.91%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    // bfs
-    vector<vector<int>>ans;
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-    int n = graph.size();    
-    vector<int>adj[n];
-    for(int i  = 0;i<n;i++)
+    // dfs
+    void solve(vector<vector<int>>&graph,vector<vector<int>>&ans,vector<int>&temp,int node)
     {
-        for(auto g : graph[i])
+        temp.push_back(node);
+        if(node == graph.size()-1)
         {
-            
-            adj[i].push_back(g);
+            ans.push_back(temp);
+    
         }
-    }
-    queue<vector<int>>q;
-    q.push({0});
-    while(q.size()>0)
-    {
-        auto  path = q.front();
-        q.pop();
-        if(path.back() == n-1)
-        {
-            ans.push_back(path);
-        }
-        int x = path.back();
-        for(int i = 0;i<adj[x].size();i++)
-        {
-            vector<int>temp = path;
-            temp.push_back(adj[x][i]);
-            q.push(temp);   
-        }
-    }
         
-    return ans;
+        else
+        {
+            for(auto x : graph[node])
+            {
+                solve(graph,ans,temp,x);
+            }
+        
+        }
+        
+        temp.pop_back();
+        
+    }
+          
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(graph,ans,temp,0);
+        return ans;
+            
         
         
     }
