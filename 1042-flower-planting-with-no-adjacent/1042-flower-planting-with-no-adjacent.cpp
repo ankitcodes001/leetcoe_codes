e05d5ff389271f48ce03d1e98f7b2b002f5659ed
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-08-04 14:10:27 +0530

    Time: 200 ms (64.57%), Space: 50.2 MB (39.63%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
+    void DFS(int i ,vector<int>adj[],vector<int>&vis,vector<int>&ans)
+    {
+        vis[i] = 1;
+        vector<int>visited_color(5,0);
+        for(auto x : adj[i])
+        {   if(vis[x] == 0)
+            DFS(x,adj,vis,ans);
+            visited_color[ans[x]] = 1;
+        }
+        
+        for(int j  = 1;j<=4;j++)
+        {
+            if(visited_color[j] == 0)
+            {
+                ans[i] = j;
+                break;
+            }
+        }
+        
+    }
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) 
     {
         vector<int>adj[n+1];
+        vector<int>vis(n+1,0);
+        vector<int>ans(n,0);
+        
         for(auto v : paths)
         {
             adj[v[0]-1].push_back(v[1]-1);
             adj[v[1]-1].push_back(v[0]-1);
         }
-        vector<int>res(n);
-        for(int i  = 0;i<n;i++)
-        {
-            bool col[5] = {};
-            for(auto j:adj[i])
-            {
-                col[res[j]] = true;
-            }
-            
-            for(int k = 1;k<=4;k++)
-            {
-                if(!col[k])
-                {
-                    res[i] = k;
-                }
-            }
         
-            
+        
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i] ==  0)
+            {
+                DFS(i,adj,vis,ans);
+            }
         }
         
-        return res;
+        return ans;
         
     }
 };
