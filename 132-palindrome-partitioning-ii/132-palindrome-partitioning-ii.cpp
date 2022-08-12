e724a74a4f5ecdb6c0e1016b056fc298302c4b3f
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-08-12 21:41:58 +0530

    Time: 871 ms (10.03%), Space: 24 MB (37.51%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-     int dp[2001][2001];
-    bool is_pal(string &s,int i, int j)
+    int dp[2001][2001];
+    int mn = INT_MAX;
+    bool is_palindrome(string &s,int i,int j)
     {
-        if(i==j)
+        if(i == j)
             return true;
         while(i<=j)
         {
-            if(s[i]!=s[j])
+            if(s[i++]!=s[j--])
                 return false;
-            i++;
-            j--;
+            
         }
-        
-        return true;
-        
-        
+        return true;  
     }
+    
     int solve(string &s,int i,int j)
     {
-        if(i >=j)
+        if(i>=j)
+        {
             return 0;
-        if(is_pal(s,i,j))
+        }
+        if(is_palindrome(s,i,j))
         {
             return dp[i][j] = 0;
         }
@@ -30,31 +30,24 @@ public:
         
         int ans  = INT_MAX;
         for(int k = i;k<j;k++)
-        { int temp;
-            if(is_pal(s,i,k))
-            {
-                temp =  1+solve(s,k+1,j);
-            }
-          ans  = min(temp,ans);
-            
-             
+        {  int temp;
+         if(is_palindrome(s,i,k))
+         {
+             temp = 1+solve(s,k+1,j);
+         }
+        ans = min(ans,temp);
         }
-         return dp[i][j] = ans;
         
         
-        
+        return dp[i][j] = ans;
     }
+    
     int minCut(string s) {
-    int n  = s.size();
-        
-    memset(dp, -1,sizeof(dp));
-    return solve(s,0,s.size()-1);
-         
-         
-         
-        
-        
-        
+    
+    memset(dp,-1,sizeof(dp));
+   
+   int n = s.size()-1; 
+    return solve(s,0,n);   
         
         
     }
