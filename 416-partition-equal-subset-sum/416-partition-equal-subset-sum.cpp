de843d78dd1544f5c4897acde40433bdd6fcfa5f
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-08-19 11:26:26 +0530

    Time: 474 ms (30.08%), Space: 27.2 MB (38.60%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,53 +1,40 @@
 class Solution {
 public:
     
-    bool subset(vector<int>&a,int sum)
-    {   int n = a.size();
-        int s = sum;
-        vector<vector<bool>>dp(n+1,vector<bool>(s+1));
-        for(int i = 0;i<=n;i++)
-        {
-            for(int j = 0;j<=s;j++)
-            {
-                if(i == 0)
-                {
-                    dp[i][j] = false;
-                }
-                if(j == 0)
-                {
-                dp[i][j] = true;
-                }
-            }
-        }
+    int dp[201][20001];
+    bool subset(vector<int>&a,int sum,int n)
+    {   
+        if(n == 0 && sum == 0)
+            return 1;
+        if(sum == 0)
+            return 1;
+        if( n == 0)
+            return 0;
+        if(dp[n][sum]!= -1)
+            return dp[n][sum];
+        if(a[n-1]> sum)
+            return dp[n][sum] = subset(a,sum,n-1);
+        else return dp[n][sum] = subset(a,sum ,n-1) || subset(a,sum-a[n-1],n-1);
+            
         
-     for(int i = 1;i<=n;i++)
-     {
-         for(int j = 0;j<=s;j++)
-         {
-             if(a[i-1]<=j)
-             {
-                 dp[i][j] = dp[i-1][j]||dp[i-1][j-a[i-1]];
-             }
-             else
-                 dp[i][j] = dp[i-1][j];
-         }
-     }
         
-        return dp[n][s];
+        
         
     }
     
     bool canPartition(vector<int>& nums) {
         
+        memset(dp,-1,sizeof(dp));
         int sum  = 0;
         for(int i = 0;i<nums.size();i++)
         {
             sum+=nums[i];
         }
-        
+     int n = nums.size();    
         if(sum%2!=0)
             return false;
-        return subset(nums,sum/2);
+    
+        return subset(nums,sum/2,n);
         
     }
 };
