bd9a02f721d38047ef13c9e7c7dcc3e7e12af870
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-08-24 12:46:46 +0530

    Time: 11 ms (71.11%), Space: 13.5 MB (84.26%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,81 +1,62 @@
 class Solution {
 public:
     
-    int firstOcc(vector<int>&a,int k)
-    {
-        int res =-1;
-        int start = 0;
-        int end = a.size()-1;
-        
-        while(start <= end)
-        {
-            int mid = start+(end-start)/2;
-            
-            if(a[mid] == k)
-            {
-                res = mid;
-                end = mid-1;
-            }
-            else if(k > a[mid])
-            {
-                start = mid+1;
-            }
-            else if( k < a[mid])
-            {
-                end = mid-1;
-            }
-            
-            
-            
-        }
-        
-        
-        return res;
-        
-        
-        
-    }
-    
-     int secondOcc(vector<int>&a,int k)
-    {
-        int res =-1;
-        int start = 0;
-        int end = a.size()-1;
-        
-        while(start <= end)
-        {
-            int mid = start+(end-start)/2;
-            
-            if(a[mid] == k)
-            {
-                res = mid;
-                start = mid+1;
-            }
-            else if(k > a[mid])
-            {
-                start = mid+1;
-            }
-            else if( k < a[mid])
-            {
-                end = mid-1;
-            }
-            
-            
-            
-        }
-        
-        
-        return res;
-        
-        
-        
-    }
+  
+int first(vector<int>&a,int k)
+{   int n = a.size();
+    int l =0 ;
+    int r = n-1;
+ int res = -1;
+   while(l<=r)
+   {
+       int mid =  l+(r-l)/2;
+       if(a[mid] == k)
+       {   res = mid;
+           r = mid-1;
+       }
+       else if(k > a[mid])
+        l = mid+1;
+       else
+           r = mid-1;
+       
+       
+   }
+  
+    return res;
+}
+
+int last( vector<int>&a,int k)
+{
+
+    int n = a.size();
+    int l =0 ;
+    int r = n-1;
+    int res = -1;
+   while(l<=r)
+   {
+       int mid =  l+(r-l)/2;
+       if(a[mid] == k)
+       {
+        res =  mid;
+        l = mid+1;
+       }
+       else if(k > a[mid])
+        l = mid+1;
+       else
+           r = mid-1;
+       
+       
+   }
+  
+    return res;
+}
+
     
     
     vector<int> searchRange(vector<int>& nums, int target) {
         
-     int a = firstOcc(nums,target);
-     int b = secondOcc(nums,target); 
+     int a = first(nums,target);
+     int b = last(nums,target); 
         
    return {a,b};
         
