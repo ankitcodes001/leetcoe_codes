d3f32524da31c0e3f135d7d5f392724b95a7c2a7
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-08-31 00:06:56 +0530

    Time: 25 ms (83.49%), Space: 13.7 MB (69.82%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        vector<int>adj[n];
+       for(auto x : p)
+       {
+           adj[x[1]].push_back(x[0]);
+       }
+       queue<int>q;
+       vector<int>indegree(n+1,0);
+       for(int i = 0;i<n;i++)
+       {
+           for(int j = 0;j<adj[i].size();j++)
+           {
+               indegree[adj[i][j]]++;
+           }
+       }
+       
+      for(int i  = 0;i<n;i++)
+      {
+          if(indegree[i] == 0)
+          {
+              cout<<i<<" ";
+              q.push(i);
+          }
+              
+      }
+        
+    vector<int>ans;
+        
+     //cout<<q.size()<<endl;   
+     while(q.size() > 0)
+     {
+         int x = q.front();
+         q.pop();
+         //cout<<x<<" " ;
+         ans.push_back(x);
+         for(int i = 0;i<adj[x].size();i++)
+         {
+             indegree[adj[x][i]]--;
+             if(indegree[adj[x][i]] == 0)
+                 q.push(adj[x][i]);
+         }
+     }
+        
+     if(ans.size() == n)
+         return ans;
+        return {};
+        
+    }
+};
