e9a2caf55b00b04c4ae808d4ea45b6bdb16e9c6c
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-05 10:57:18 +0530

    Time: 744 ms (33.33%), Space: 57.1 MB (33.33%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void bitsmap(vector<int>&bits,int x,int op)
+    {
+        for(int i  = 0;i<bits.size();i++)
+        {
+            if(x & (1 <<i))
+            {
+                bits[i]+=op;
+            }
+        }
+    }
+    bool valid(vector<int>&bits)
+    {
+        for(int i =0;i<bits.size();i++)
+        {
+            if(bits[i] > 1)
+                return false;
+        }
+        return true;
+    }
+    int longestNiceSubarray(vector<int>& nums) {
+     int n = nums.size();   
+     int i  = 0;
+     int j  = 0;
+        int ans = 0;
+     vector<int>bits(32,0);   
+        
+     while(j < n)
+     {
+        bitsmap(bits,nums[j],1);
+        while(valid(bits) == false)
+        {
+            bitsmap(bits,nums[i],-1);
+            i++;
+        }
+        ans =  max(ans,j-i+1);
+         j++;
+     }
+     return ans;   
+      
+    }
+};
