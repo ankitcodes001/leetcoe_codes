f16ce72e105fd9054977e75dd90af75f0478c26b
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-21 23:53:27 +0530

    Time: 40 ms (21.47%), Space: 19.9 MB (61.26%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,31 +5,24 @@ public:
         //leftmax[i]
         //rightmax[i]
         //ans+= min(l[i],r[i])-h[i] 
-        int n = h.size();
-        int l[n];
-        l[0] = h[0];
-        
-        int r[n];
-        r[n-1] = h[n-1];
-        
-        for(int i = 1;i<n;i++)
-        {
-            l[i] = max(l[i-1],h[i]);
-        }
-        for(int i = n-2;i>=0;i--)
-        {
-            r[i] = max(r[i+1],h[i]);
-        }
-       
-        int ans = 0;
-        
-        for(int i = 0;i<n;i++)
-        {
-            ans+=(min(l[i],r[i])-h[i]);
-        }
-        
-        return ans;
-        
-        
+       int n = h.size();
+       int left_max[n];
+       left_max[0] = h[0];
+       int right_max[n];
+       right_max[n-1] = h[n-1];
+       for(int i  =1;i<n;i++)
+       {
+           left_max[i] = max(h[i],left_max[i-1]);
+       }
+       for(int i  = n-2;i>=0;i--)
+       {
+           right_max[i] = max(h[i],right_max[i+1]);
+       }
+       int ans  = 0;
+       for(int i  = 0;i<n;i++)
+       {
+           ans+= (min(left_max[i],right_max[i]) - h[i]);
+       }
+       return ans; 
     }
 };
