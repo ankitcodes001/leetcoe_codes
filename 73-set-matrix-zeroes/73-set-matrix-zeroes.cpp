d9ea7a326721cd05b2dc7f398eebf59dac1c6628
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-23 19:27:18 +0530

    Time: 17 ms (81.62%), Space: 13.1 MB (81.99%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        set<pair<int,int>>s;
-        int n = mat.size();
-        int m = mat[0].size();
-        for(int i =0;i<n;i++)
+       set<pair<int,int>>s;
+    for(int i = 0;i<mat.size();i++)
+    {
+        for(int j  = 0;j<mat[0].size();j++)
         {
-            for(int j=0;j<m;j++)
+            if(mat[i][j] == 0)
             {
-                if(mat[i][j] == 0)
-                {
-                    s.insert({i,j});
-                }
-                
+                s.insert({i,j});
             }
-                
         }
-        
-      for(auto  it : s)
-      {
-          int r = it.first;
-          int c = it.second;
-          
-          for(int i = 0;i<n;i++)
-          {
-              mat[i][c] = 0;
-          }
-          for(int i = 0;i<m;i++)
-          {
-              mat[r][i] = 0;
-          }
-          
-      }
-        
+    }
+    int n = mat.size();
+    int m = mat[0].size();
+    
+    for(auto it : s)
+    {
+        int r = it.first;
+        int c = it.second;
+        for(int i  = 0;i<m;i++)
+        {
+            mat[r][i] = 0;
+            
+        }
+        for(int i = 0;i<n;i++)
+        {
+            mat[i][c] = 0;
+        }
+    }
         
     }
 };
