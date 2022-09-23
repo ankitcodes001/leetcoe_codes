74192d324d9ee62cafbc0abfe99639cdc333c5a9
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-23 12:02:56 +0530

    Time: 358 ms (13.93%), Space: 31.1 MB (89.65%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        
-        
-//         for(auto x : a)
-//             cout<<x<<" ";
-//         for(int i  = 0;i<a.size();i++)
-//         {
-//             if(i!=a[i])
-//                 return i;
-//         }
-//         return a[a.size()-1]+1;
-     int i  = 0;
-     int n = nums.size();
-    while(i<nums.size())
-    {
-        if(nums[i]>=1 && nums[i]<=n && nums[i]!=  i+1 && nums[nums[i]-1]!= nums[i])
+    
+    // sort in such a way that from 1 should at their respective position 
+   // and then simple compare with i+1
+   // return ans;     
+   int i = 0;
+        int n = nums.size();
+        while(i<nums.size())
         {
-            swap(nums[i],nums[nums[i]-1]);
+            if(nums[i]>=1 && nums[i]<=n && nums[i]!= nums[nums[i]-1])
+            {
+                swap(nums[i],nums[nums[i]-1]);
+            }
+            else
+                i++;
         }
-        else
-        {
-            i++;
-        }
-    }
+     for(auto x : nums)
+         cout<<x<<" ";
     for(int j  = 0;j<nums.size();j++)
          if(j+1!= nums[j])
                 return j+1;
