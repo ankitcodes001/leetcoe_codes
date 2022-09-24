26bea493a882b301d12a9bfd208e76a300b85563
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-24 16:39:53 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (61.23%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -4,13 +4,14 @@ public:
     vector<int>curr;
     void subset(int start,int end ,int k ,int target)
     {
-        if(target == 0 &&  curr.size() == k)
+    // {
+    //     if(start == end || target<0)
+    //         return ;
+        if(target == 0 && curr.size() == k)
         {
-            
             ans.push_back(curr);
             return;
         }
-        
         
         for(int i = start;i<=end;i++)
         {
@@ -18,14 +19,9 @@ public:
             subset(i+1,end,k,target-i);
             curr.pop_back();
         }
-        
-        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        
-        subset(1,9,k,n);
-        return ans;
-        
-        
+       subset(1,9,k,n);
+        return ans;    
     }
 };
