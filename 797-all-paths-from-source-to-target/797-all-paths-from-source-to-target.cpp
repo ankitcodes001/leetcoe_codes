72ce5fc391a7e3d698603948021dd5300f929735
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-09-26 06:39:28 +0530

    Time: 51 ms (20.79%), Space: 14.5 MB (37.79%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    // dfs
-    void solve(vector<vector<int>>&graph,vector<vector<int>>&ans,vector<int>&temp,int node)
-    {
-        temp.push_back(node);
-        if(node == graph.size()-1)
-        {
-            ans.push_back(temp);
-    
-        }
-        
-        else
-        {
-            for(auto x : graph[node])
-            {
-                solve(graph,ans,temp,x);
-            }
-        
-        }
-        
-        temp.pop_back();
-        
-    }
+   vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+     queue<vector<int>>q;
+     q.push({0});
+     vector<vector<int>>ans;  
+     while(q.size() > 0)
+     {
+         auto it = q.front();
+         q.pop();
+         
+         for(auto x : graph[it.back()])
+         {   
+             it.push_back(x);
           
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        solve(graph,ans,temp,0);
-        return ans;
-            
-        
-        
+             if(x == graph.size()-1)
+             {
+                 ans.push_back(it);
+             }
+              else
+              {
+                  q.push(it);
+                  
+              }
+              it.pop_back();
+         }
+     }
+  
+       return ans;
     }
 };
