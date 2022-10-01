536623f2fceea336dc481ad02debfecc64c8139e
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-01 07:41:01 +0530

    Time: 27 ms (86.50%), Space: 6.4 MB (51.76%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -4,7 +4,22 @@ public:
     
     bool is_safe(vector<vector<char>>&board,int row,int col,char x)
     {
-        int n_row = 3*(row/3);
+//         int n_row = 3*(row/3);
+//         int n_col = 3*(col/3);
+        
+//         for(int i = 0;i<9;i++)
+//         {
+//             if(board[row][i] == x)
+//                 return false;
+            
+//             if(board[i][col] == x)
+//                 return false;
+            
+//             if(board[n_row+(i/3)][n_col+(i%3)] == x)
+//                 return false;
+//         }
+//         return true;
+         int n_row = 3*(row/3);
         int n_col = 3*(col/3);
         
         for(int i = 0;i<9;i++)
@@ -17,30 +32,36 @@ public:
                 return false;
         }
         return true;
+
     }
-
    
-bool solve(vector<vector<char>>&board)
+    bool solve(vector<vector<char>>&board)
     {
-        for (int i = 0; i <9; i++) {
-            for (int j = 0; j <9; j++) {
-              if (board[i][j] == '.') {
-                for (char c = '1'; c <= '9'; c++) {
-                  if (is_safe(board,i, j, c)) {
-                    board[i][j] = c;
+       for(int i = 0;i<9;i++)
+       {
+           for(int j = 0;j<9;j++)
+           {   
+               if(board[i][j] == '.')
+               {
+                       for(char c = '1';c<='9';c++)
+                       {
+                           if(is_safe(board,i,j,c))
+                           {   
+                             board[i][j] = c;
 
-                    if (solve(board))
-                      return true;
-                    else
-                      board[i][j] = '.';
-                  }
-                }
-
-                return false;
-              }
-            }
-          }
-      return true; 
+                            if(solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                               
+                           }
+                       }  
+                  return false;
+               }
+           }
+       }
+        return true;
+      
     }
   void solveSudoku(vector<vector<char>>& board) {
          solve(board);
