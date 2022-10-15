3cb765d13f260e56333361edfd89c2752549ddd7
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-16 04:07:47 +0530

    Time: 1055 ms (44.65%), Space: 192.4 MB (57.59%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
@@ -1,15 +1,16 @@
 #define ll long long
 class Solution {
 public:
-    ll mod = 1e9+7;
-    vector<ll>ans,sz,fact;
+    vector<ll>sz,ans,fact;
     vector<int>adj[100005];
+    ll mod=1e9+7;
+    
     ll power(ll a,ll b)
     {
         if(b == 0)
             return 1;
-        ll temp = power(a,b/2);
-        ll even  = (temp*temp)%mod;;
+        ll  temp = power(a,b/2);
+        ll even = (temp*temp)%mod;
         if(b%2 == 0)
             return even;
         else
@@ -21,43 +22,48 @@ public:
         return power(x,mod-2);
     }
     
+    
     void precompute(ll n)
-    {
-        ll prod = 1;
-        fact[0] = 1;
-        fact[1] = 1;
-        for(int i = 2;i<=n;i++)
-        {
-            prod = (i*prod)%mod;
-            fact[i] = prod;
-        }
+    { ll prod = 1;
+      fact[0] = 1;
+      fact[1] = 1;
+      for(int i = 2;i<=n;i++)
+      {
+          prod = (i*prod)%mod;
+          fact[i] = prod;
+      }
     }
     
-    void dfs(ll src)
+    void dfs(int src)
     {
-        sz[src] = 1;
         ans[src] = 1;
+        sz[src] = 1;
+        
+        
         for(auto e:adj[src])
         {
             dfs(e);
-            sz[src] = (sz[src]+sz[e])%mod;
+            sz[src] = (sz[src] + sz[e])%mod;
             ans[src] = (ans[src]*ans[e])%mod;
-            ans[src] = (ans[src]*modinv(fact[sz[e]]))%mod;
+            ans[src] = (ans[src] * modinv(fact[sz[e]]))%mod;
         }
-        
-        ans[src] = (ans[src]*fact[sz[src]-1])%mod;
+        ans[src] = (ans[src] * fact[sz[src]-1])%mod;
     }
+    
+    
     
     int waysToBuildRooms(vector<int>& p) {
     for(int i = 1;i<p.size();i++)
-    {
         adj[p[i]].push_back(i);
-    }
+        
     ans.resize(p.size());
-    fact.resize(p.size()+1);
     sz.resize(p.size());
+    fact.resize(p.size()+1);
     precompute(p.size());
     dfs(0);
     return ans[0];
+        
+        
+        
     }
 };
