d423aa9393b637a0c9ae341096c9c397b0424ff9
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-16 04:31:39 +0530

    Time: 14 ms (65.42%), Space: 7.1 MB (57.35%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,34 +1,33 @@
 #define ll long long
 class Solution {
 public:
-   ll mod = 1e9+7;
+    ll  dp[52][52][52];
     ll row,col;
-    ll dp[52][52][52];
-    int solve(int i,int j,int k,int max_move)
+    ll mod = 1e9+7;
+    
+    ll solve(int i ,int j,int move,int max_move)
     {
-        if(i<=-1 || j<=-1|| i>=row || j>=col)
+        if(i<=-1 || i>=row || j<=-1 || j>=col)
             return 1;
-        if(k == max_move)
+        if(move == max_move)
             return 0;
-        if(dp[i][j][k]!= -1)
-            return dp[i][j][k];
         
+        if(dp[i][j][move]!= -1)
+            return dp[i][j][move];
         
-       int a = solve(i+1,j,k+1,max_move);
-       int b = solve(i-1,j,k+1,max_move);
-       int c =  solve(i,j-1,k+1,max_move);
-       int d = solve(i,j+1,k+1,max_move);
+        ll a = solve(i+1,j,move+1,max_move);
+        ll b = solve(i-1,j,move+1,max_move);
+        ll c = solve(i,j-1,move+1,max_move);
+        ll d = solve(i,j+1,move+1,max_move);
         
-      return dp[i][j][k] = (a%mod + b%mod + c%mod + d%mod)%mod;  
-        
+        return dp[i][j][move] = (a%mod + b%mod + c%mod + d%mod)%mod;
         
     }
+    
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-       row = m;
-        col = n;
-        memset(dp,-1,sizeof(dp));
-        ll ans = solve(startRow,startColumn,0,maxMove);
-    return ans;
-        
+    row = m;
+    col = n;
+    memset(dp,-1,sizeof(dp));
+    return solve(startRow,startColumn,0,maxMove);
     }
 };
