cbc79fc00cdbba0dfacc83ee6c3b235246f5dcc8
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-15 22:31:01 +0530

    Time: 1048 ms (44.20%), Space: 192.3 MB (57.59%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
@@ -1,26 +1,24 @@
- #define ll long long 
+#define ll long long
 class Solution {
 public:
     ll mod = 1e9+7;
     vector<ll>ans,sz,fact;
     vector<int>adj[100005];
-    
     ll power(ll a,ll b)
     {
         if(b == 0)
             return 1;
         ll temp = power(a,b/2);
-        ll even = (temp*temp)%mod;
+        ll even  = (temp*temp)%mod;;
         if(b%2 == 0)
             return even;
         else
             return (even*a)%mod;
     }
     
-    ll modInv(ll x)
+    ll modinv(ll x)
     {
         return power(x,mod-2);
-        
     }
     
     void precompute(ll n)
@@ -35,33 +33,31 @@ public:
         }
     }
     
-    
     void dfs(ll src)
     {
-        ans[src] = 1;
         sz[src] = 1;
+        ans[src] = 1;
         for(auto e:adj[src])
-        {   dfs(e);
-            sz[src] = (sz[src] + sz[e])%mod;
+        {
+            dfs(e);
+            sz[src] = (sz[src]+sz[e])%mod;
             ans[src] = (ans[src]*ans[e])%mod;
-            ans[src] = (ans[src]*modInv(fact[sz[e]]))%mod;
+            ans[src] = (ans[src]*modinv(fact[sz[e]]))%mod;
         }
+        
         ans[src] = (ans[src]*fact[sz[src]-1])%mod;
     }
+    
     int waysToBuildRooms(vector<int>& p) {
-        
     for(int i = 1;i<p.size();i++)
     {
         adj[p[i]].push_back(i);
     }
-        
     ans.resize(p.size());
-    sz.resize(p.size());
     fact.resize(p.size()+1);
+    sz.resize(p.size());
     precompute(p.size());
     dfs(0);
-        
     return ans[0];
     }
 };
-
