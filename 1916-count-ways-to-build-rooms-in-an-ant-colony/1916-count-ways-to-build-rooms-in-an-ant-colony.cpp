bf41a2429fdf20d4187ddbdaea84564bbc2397ca
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-15 22:15:51 +0530

    Time: 1676 ms (18.31%), Space: 192.3 MB (58.04%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.cpp
@@ -1,53 +1,67 @@
+ #define ll long long 
 class Solution {
 public:
-    static const int mod=1e9+7;
+    ll mod = 1e9+7;
+    vector<ll>ans,sz,fact;
     vector<int>adj[100005];
-    vector<long long>ans,sz;
-    vector<long long>fact;
-    long long power(long long a,long long b){
-        
+    
+    ll power(ll a,ll b)
+    {
         if(b == 0)
             return 1;
-        long long temp = power(a,b/2);
-        long long even = (temp*temp)%mod;
+        ll temp = power(a,b/2);
+        ll even = (temp*temp)%mod;
         if(b%2 == 0)
             return even;
         else
-            return (a*even)%mod;
-        
-        
-        
+            return (even*a)%mod;
     }
-    long long modinv(long long x){
+    
+    ll modInv(ll x)
+    {
         return power(x,mod-2);
+        
     }
-    void precompute(int n){
-        long long prod=1;
-        fact[1]=1;
-        fact[0]=1;
-        for(long long i=2;i<=n;i++){
-            prod=(prod*i)%mod;
-            fact[i]=prod;
+    
+    void precompute(ll n)
+    {
+        ll prod = 1;
+        fact[0] = 1;
+        fact[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            prod = (i*prod)%mod;
+            fact[i] = prod;
         }
     }
-    void dfs(int src){
-        ans[src]=1;
-        sz[src]=1;
-        for(auto edge:adj[src]){
-            dfs(edge);
-            sz[src]=(sz[src]+sz[edge])%mod;
-            ans[src]=(ans[src]*ans[edge])%mod;
-            ans[src]=(ans[src]*modinv(fact[sz[edge]]))%mod;
+    
+    
+    void dfs(ll src)
+    {
+        ans[src] = 1;
+        sz[src] = 1;
+        for(auto e:adj[src])
+        {   dfs(e);
+            sz[src] = (sz[src] + sz[e])%mod;
+            ans[src] = (ans[src]*ans[e])%mod;
+            ans[src] = (ans[src]*modInv(fact[sz[e]]))%mod;
         }
-        ans[src]=(ans[src]*fact[sz[src]-1])%mod;
+        ans[src] = (ans[src]*fact[sz[src]-1])%mod;
     }
-    int waysToBuildRooms(vector<int>& prevRoom) {
-        for(int i=1;i<prevRoom.size();i++) adj[prevRoom[i]].push_back(i);
-        ans.resize(prevRoom.size());
-        sz.resize(prevRoom.size());
-        fact.resize(prevRoom.size()+1);
-        precompute(prevRoom.size());
-        dfs(0);
-        return ans[0];
+    int waysToBuildRooms(vector<int>& p) {
+        
+    for(int i = 1;i<p.size();i++)
+    {
+        adj[p[i]].push_back(i);
+    }
+        
+    ans.resize(p.size());
+    sz.resize(p.size());
+    fact.resize(p.size()+1);
+    precompute(p.size());
+    dfs(0);
+        
+    return ans[0];
     }
 };
+
