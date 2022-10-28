c2d0ec660fb0a832d1fe5daf5df99d97c9173cfa
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-28 19:04:28 +0530

    Time: 129 ms (65.82%), Space: 19 MB (67.67%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -7,35 +7,45 @@ public:
         vector<vector<int>> vis(n, vector<int>(m, INT_MAX));
         vis[0][0] = 0;
         int steps = 0;
+        
         queue<vector<int>> q;
         q.push({0,0,0});
         
         while(!q.empty())
         {
-            int size = q.size();
-            for(int i=0; i<size; i++)
+            int sz = q.size();
+            for(int i = 0;i<sz;i++)
             {
-                vector<int> current = q.front();
+                vector<int>curr = q.front();
                 q.pop();
-                int x = current[0], y = current[1];
+                
+                int x = curr[0];
+                int y = curr[1];
                 
                 if(x == n-1 && y == m-1)
                     return steps;
                 
-                for(int j=0; j<4; j++)
+                for(int j = 0;j<dir.size();j++)
                 {
-                    int new_x = x+dir[j][0], new_y = y+dir[j][1];
-                    if(new_x>=0 && new_y>=0 && new_x<n && new_y<m)
+                    int nx = x+dir[j][0];
+                    int ny = y+dir[j][1];
+                    
+                    if(nx>=0 && nx<n && ny>=0 && ny<m)
                     {
-                        int obs = current[2] + grid[new_x][new_y];
-                        if(obs>=vis[new_x][new_y] || obs>k)
+                        int obs = curr[2]+grid[nx][ny];
+                        
+                        if(obs>=vis[nx][ny] || obs > k)
                             continue;
-                        vis[new_x][new_y] = obs;
-                        q.push({new_x, new_y, obs});
+                        vis[nx][ny] = obs;
+                        q.push({nx,ny,obs});
+                        
                     }
                 }
+    
             }
-            steps++;
+             steps++;
+            
+             
         }
         return -1;
         
