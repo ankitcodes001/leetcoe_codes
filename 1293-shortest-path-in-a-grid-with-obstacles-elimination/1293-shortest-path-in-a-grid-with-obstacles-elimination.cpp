a2453b6842762c1aa4d9d09436d972291c503f86
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-28 19:12:55 +0530

    Time: 146 ms (61.65%), Space: 20.2 MB (63.07%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -2,52 +2,47 @@ class Solution {
 public: 
     int shortestPath(vector<vector<int>>& grid, int k) {
         
-        vector<vector<int>>dir = {{1,0},{0,1},{-1,0},{0,-1}};    
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, INT_MAX));
-        vis[0][0] = 0;
-        int steps = 0;
-        
-        queue<vector<int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        vector<vector<int>>vis(n,vector<int>(m,INT_MAX));
+        queue<vector<int>>q;
         q.push({0,0,0});
+        vis[0][0] = 0;
         
-        while(!q.empty())
+        int step = 0;
+        
+        while(q.size()>0)
         {
             int sz = q.size();
             for(int i = 0;i<sz;i++)
             {
                 vector<int>curr = q.front();
-                q.pop();
-                
                 int x = curr[0];
                 int y = curr[1];
-                
+                q.pop();
                 if(x == n-1 && y == m-1)
-                    return steps;
+                    return step;
                 
                 for(int j = 0;j<dir.size();j++)
                 {
-                    int nx = x+dir[j][0];
-                    int ny = y+dir[j][1];
+                    int nx = dir[j][0]+x;
+                    int ny = dir[j][1]+y;
                     
                     if(nx>=0 && nx<n && ny>=0 && ny<m)
                     {
                         int obs = curr[2]+grid[nx][ny];
-                        
-                        if(obs>=vis[nx][ny] || obs > k)
+                        if(obs >= vis[nx][ny] || obs > k)
                             continue;
                         vis[nx][ny] = obs;
-                        q.push({nx,ny,obs});
-                        
+                        q.push({nx,ny,obs});     
                     }
                 }
-    
+                
             }
-             steps++;
-            
-             
+            step++;
         }
-        return -1;
-        
+    
+    return -1;
     }
 };
