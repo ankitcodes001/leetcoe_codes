436840fcae044e0faa7465a393669063384c58a4
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-29 20:11:35 +0530

    Time: 152 ms (53.08%), Space: 11.3 MB (53.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-           int n = s.size();
-           if(n == 1)
-               return s;
+        int n = s.size();
+        if(n==1)
+            return s;
         int dp[n][n];
         memset(dp,0,sizeof(dp));
-        int start = 0,end=1;
         for(int i = 0;i<n;i++)
             dp[i][i] = 1;
-        int i,c=1;
         
-        for(int i = 0;c<n;i=0,c++)
+        int start = 0,end =1;
+        int c =1;
+        for(int i =0;c<n;i=0,c++)
         {
-            for(int j = i+c;j<n;i++,j++)
+            for(int j = i+c;j<n;j++,i++)
             {
-                if(s[i] == s[j] and (j-i==1 or dp[i+1][j-1]))
+                if(s[i] == s[j] and (j-i == 1 or dp[i+1][j-1]))
                 {
                     dp[i][j] = 1;
                     start = i;
@@ -23,34 +23,7 @@ public:
                 }
             }
         }
-        string t = s.substr(start,end);
-        cout<<t.size();
+        
         return s.substr(start,end);
-    
-//         int length = s.size();
-//         if(length==1)
-//             return s;
-        
-//         int dp[length][length];
-//         int start=0,end=1;
-//         memset(dp,0,sizeof(dp));
-        
-//         for(int i=0;i<length;i++)
-//             dp[i][i]=1;  
-//         int i,c = 1;    
-//         for(i=0;c<length;i=0,c++) {   
-            
-//             for(int j=i+c;j<length;j++,i++) { 
-                
-//                 if(s[i]==s[j] and (j-i==1 or dp[i+1][j-1])) {      
-//                     dp[i][j]=1;
-//                     start=i;
-//                     end=j-i+1;
-//                 }
-//             }   
-//         }
-
-//         return s.substr(start,end);
-        
     }
 };
