9ce7abad10e832e1cc864fdc42d712254846a924
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-30 17:12:01 +0530

    Time: 1076 ms (5.05%), Space: 172.3 MB (8.61%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    int balancedString(string s) {
-           int n=s.size();
-        int q=0,w=0,e=0,r=0;
-        for(auto &i:s){
-            if(i=='Q')q++;
-            if(i=='W')w++;
-            if(i=='E')e++;
-            if(i=='R')r++;
+    bool is_bal(unordered_map<char,int>mp,int k)
+    {
+        for(auto x:mp)
+        {
+            if(x.second > k)
+                return false;
         }
-        //count of faulty q,w,e,r which needs to be replaced
-        int fq=0,fw=0,fe=0,fr=0;
-        if(q>n/4)fq=q-n/4;
-        if(w>n/4)fw=w-n/4;
-        if(e>n/4)fe=e-n/4;
-        if(r>n/4)fr=r-n/4;
-        int i=0,j=0;
-        q=0,w=0,e=0,r=0;
-        int res=INT_MAX;
-        while(j<n){
-            if(s[j]=='Q')q++;
-            if(s[j]=='W')w++;
-            if(s[j]=='E')e++;
-            if(s[j]=='R')r++;
-            
-            
-            while((s[i]=='Q'&&q>fq)||(s[i]=='W'&&w>fw)||(s[i]=='E'&&e>fe)||(s[i]=='R'&&r>fr)){
-                if(s[i]=='Q')q--;
-                if(s[i]=='W')w--;
-                if(s[i]=='E')e--;
-                if(s[i]=='R')r--;
-                i++;
-            }
-            if(q>=fq&&w>=fw&&e>=fe&&r>=fr)res=min(res,j-i+1);
-            j++;
-        }
-        return res;
+        return true;
     }
-    
-
+    int balancedString(string s) {
+     unordered_map<char,int> mp;
+        for(auto &c:s){
+            mp[c]++;
+        }
+        int n=s.length();
+        int limit=n/4;
+        int ans=n;
+        if(is_bal(mp,limit))return 0;
+        int i=0,j=0;
+        while(i<n){
+            mp[s[i]]--;   //checking inside window j-i
+            if(is_bal(mp,limit)){
+                while(j<=i && is_bal(mp,limit)){
+                    ans=min(ans,i-j+1);    
+                    mp[s[j]]++;  //when we drop j off window, we should
+                    //increase the count which was decreased earlier
+                    j++; 
+                }
+            }
+            i++;
+        }
+        return ans;
+    }
 };
+    
