db3b8530e3a637879064a627ac92f97f04b147db
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-30 17:15:21 +0530

    Time: 1032 ms (5.05%), Space: 172.2 MB (8.61%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -10,28 +10,35 @@ public:
         return true;
     }
     int balancedString(string s) {
-     unordered_map<char,int> mp;
-        for(auto &c:s){
-            mp[c]++;
-        }
-        int n=s.length();
-        int limit=n/4;
-        int ans=n;
-        if(is_bal(mp,limit))return 0;
-        int i=0,j=0;
-        while(i<n){
-            mp[s[i]]--;   //checking inside window j-i
-            if(is_bal(mp,limit)){
-                while(j<=i && is_bal(mp,limit)){
-                    ans=min(ans,i-j+1);    
-                    mp[s[j]]++;  //when we drop j off window, we should
-                    //increase the count which was decreased earlier
-                    j++; 
-                }
+    unordered_map<char,int>mp;
+    for(auto c:s)
+    {
+        mp[c]++;
+    }
+    int n = s.size();
+    int k = n/4;
+    int ans = n;
+    int i = 0,j=0;
+    if(is_bal(mp,k)) return 0;
+    while(j<n)
+    {
+        mp[s[j]]--;
+        if(is_bal(mp,k))
+        {
+            while(i<=j && is_bal(mp,k))
+            {   ans = min(ans,j-i+1);
+                mp[s[i]]++;
+               i++;
             }
-            i++;
+            
         }
-        return ans;
+        j++;
+    }
+
+        
+        
+    return ans;
+        
     }
 };
     
