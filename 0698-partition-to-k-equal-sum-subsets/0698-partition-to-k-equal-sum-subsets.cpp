5a34b625eb18a81bef2f70515417b30f7ab27184
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-30 17:42:43 +0530

    Time: 1847 ms (24.45%), Space: 9.5 MB (42.73%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    
-  bool check(vector<int>&nums, vector<bool>&vis,int start,int curr,int target,int k)
-  {
-      if(k ==  1)
-          return true;
-      if(start>=nums.size())
-          return false;
-      if(curr == target)
-      {
-          if(check(nums,vis,0,0,target,k-1))
-              return true;
-      }
-      
-      for(int i = start;i<nums.size();i++)
-      {
-          if(vis[i]== false && curr+nums[i]<=target)
-          {
-              vis[i] = true;
-              if(check(nums,vis,i+1,curr+nums[i],target,k))
-                  return true;
-              vis[i] = false;
-          }
-      }
-      
-      return false;
-  }
+   bool solve(vector<int>&a,vector<bool>&vis,int idx,int curr,int tar,int k)
+   {
+       if(k == 1)
+           return true;
+       if(idx>=a.size())
+           return false;
+       if(curr == tar)
+       {
+           if(solve(a,vis,0,0,tar,k-1))
+               return true;
+       }
+       
+       for(int i = idx;i<a.size();i++)
+       {
+           if(vis[i]==false && curr+a[i]<=tar)
+           {
+               vis[i] = true;
+               if(solve(a,vis,i+1,curr+a[i],tar,k))
+                   return true;
+               vis[i] = false;
+           }
+       }
+       
+       return false;
+   }
     
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         
@@ -37,6 +36,6 @@ public:
             return false;
         vector<bool>vis(nums.size(),false);
         
-        return check(nums,vis,0,0,sum/k,k);
+        return solve(nums,vis,0,0,sum/k,k);
     }
 };
