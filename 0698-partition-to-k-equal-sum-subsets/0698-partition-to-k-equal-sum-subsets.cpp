ec4e62af8d602277feb5bcc8ef3bf195f80a7b7c
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-30 17:30:37 +0530

    Time: 1277 ms (39.17%), Space: 9.5 MB (42.73%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+  bool check(vector<int>&nums, vector<bool>&vis,int start,int curr,int target,int k)
+  {
+      if(k == 1)
+          return true;
+     if(start>=nums.size()) //This line is important to avoid tle
+          return false;
+      
+      if(curr == target)
+      {
+        return check(nums,vis,0,0,target,k-1);
+      }
+      
+      for(int i = start;i<nums.size();i++)
+      {
+          if(!vis[i] && curr+nums[i]<= target)
+          {
+           vis[i] = true;
+          
+          if(check(nums,vis,i+1,curr+nums[i],target,k)) return true;
+          vis[i]= false;    
+              
+          }       
+      }
+      
+      return false;
+  }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        
+        int sum = 0;
+        for(int i = 0;i<nums.size();i++)
+            sum+=nums[i];
+       
+        if(sum%k!=0)
+            return false;
+        vector<bool>vis(nums.size(),false);
+        
+        return check(nums,vis,0,0,sum/k,k);
+        
+        
+        
+        
+    }
+};
