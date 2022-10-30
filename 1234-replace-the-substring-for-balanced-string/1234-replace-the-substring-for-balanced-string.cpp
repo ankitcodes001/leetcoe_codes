8c2633f71308ce8998bbfb9e40f06db30c8265a3
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-10-30 17:19:45 +0530

    Time: 1039 ms (5.05%), Space: 172.3 MB (8.61%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     bool is_bal(unordered_map<char,int>mp,int k)
     {
         for(auto x:mp)
@@ -11,34 +12,34 @@ public:
     }
     int balancedString(string s) {
     unordered_map<char,int>mp;
-    for(auto c:s)
-    {
-        mp[c]++;
-    }
+    for(auto x:s)
+        mp[x]++;
+    
     int n = s.size();
     int k = n/4;
     int ans = n;
-    int i = 0,j=0;
+    
     if(is_bal(mp,k)) return 0;
+        
+    int i = 0,j=0;
     while(j<n)
     {
         mp[s[j]]--;
         if(is_bal(mp,k))
         {
             while(i<=j && is_bal(mp,k))
-            {   ans = min(ans,j-i+1);
+            {
+                ans = min(ans,j-i+1);
                 mp[s[i]]++;
-               i++;
+                i++;
             }
+            
             
         }
         j++;
     }
-
         
-        
-    return ans;
-        
+   return ans;
     }
 };
     
