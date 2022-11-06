01c9566b44c580f277dc2e15da93dfd989daacf2
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-11-06 07:46:21 +0530

    Time: 1538 ms (10.55%), Space: 256.6 MB (6.81%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        MergesortHelper(nums, 0, size(nums) - 1);
-        return nums;
-    }
-private:
-    void MergeTwoSortedArray(vector<int>& nums, int left, int mid, int right) {
-        vector<int> sorted;
-        sorted.reserve(right - left + 1);
-        
-        int k = 0;
-        int i = left;
-        int j = mid + 1;
-        
-        while (i <= mid && j <= right) {
-            if (nums[i] < nums[j]) {
-                sorted[k++] = nums[i++];
-            } else {
-                sorted[k++] = nums[j++];
-            }
-        }
-        
-        while (i <= mid) {
-            sorted[k++] = nums[i++];
-        }
-        
-        while (j <= right) {
-            sorted[k++] = nums[j++];
-        }
-        
-        while (--k >= 0) {
-            nums[right--] = sorted[k];
-        }
-    }
-    
-    void MergesortHelper(vector<int>& nums, int left, int right) {
-        if (left == right) {
+    void merge(vector<int>&nums,int l,int mid,int r)
+    {
+        if(l>=r)
             return;
+        int i=l;
+        int j=mid+1;
+        vector<int>sorted;
+        while(i<=mid&&j<=r)
+        {
+            if(nums[i]<=nums[j])
+                sorted.push_back(nums[i]),i++;
+            else
+                sorted.push_back(nums[j]),j++;
         }
-        int mid = left + (right - left) / 2;
-        MergesortHelper(nums, left, mid);
-        MergesortHelper(nums, mid + 1, right);
-        MergeTwoSortedArray(nums, left, mid, right);
+        if(i>mid)
+        {
+            while(j<=r)
+                sorted.push_back(nums[j]),j++;
+        }
+        else
+        {
+            while(i<=mid)
+                sorted.push_back(nums[i]),i++;
+        }
+        int k=0;
+        for(int i=l;i<=r;i++)
+        {
+            nums[i]=sorted[k];
+            k++;
+        }
+    }
+    void mergesort(int start,int end,vector<int>&nums)
+    {
+        if(start >= end)
+            return;
+            
+        int mid = (start+end)/2;
+        mergesort(start,mid,nums);
+        mergesort(mid+1,end,nums);
+        merge(nums,start,mid,end);
+        
+    }
+    vector<int> sortArray(vector<int>& nums) {
+    mergesort(0,nums.size()-1,nums);
+    return nums;
+        
     }
 };
