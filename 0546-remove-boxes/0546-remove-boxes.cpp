dc2a882d7884465fdbd7533e98b4c16b8633eb04
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2022-11-08 13:52:16 +0530

    Time: 449 ms (38.31%), Space: 12.6 MB (57.06%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int dp[102][102][102];
+      int dp[105][105][105];
     
-    int solve(vector<int>&box,int left,int right,int streak)
+    int dfs(vector<int>& arr, int left, int right, int streak)
     {
-     int ans;
-     if(left > right) return 0;
         
-     if(dp[left][right][streak]!= -1)
-        return dp[left][right][streak];
-     
-      if(left == right)
-          return (streak+1)*(streak+1);
+        if(left > right)
+            return 0;
+                
+        if(dp[left][right][streak] != -1)
+            return dp[left][right][streak];
         
-      ans = (streak+1)*(streak+1)+solve(box,left+1,right,0);
-     
-    for(int i = left+1 ; i<=right ;i++)
-    {
-        if(box[left] == box[i])
+        int ans = 0;
+        ans = (streak+1)*(streak+1)+dfs(arr,left+1,right,0);
+        for(int i = left+1;i<=right;i++)
         {
-            ans = max(ans,solve(box,left+1,i-1,0)+solve(box,i,right,streak+1));
+            if(arr[i] == arr[left])
+            {
+                ans = max(ans,dfs(arr,left+1,i-1,0)+dfs(arr,i,right,streak+1));
+            }
         }
+            
+        return dp[left][right][streak] = ans;
     }
-        
-    dp[left][right][streak] = ans;    
-        
-     return ans;
-    }
+    
     int removeBoxes(vector<int>& boxes) {
-       memset(dp, -1,sizeof(dp));
+        
         int n = boxes.size();
-       return solve(boxes,0,n-1,0); 
+        
+        memset(dp, -1, sizeof(dp));
+        
+        return dfs(boxes, 0, n - 1, 0);
     }
 };
